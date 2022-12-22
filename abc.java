3ffcd1dce71a31f43a42d58035bf8c6ce58a6504
ASHOKJAYA <bapathuashokreddy@gmail.com>
2022-12-22 12:33:40 +0530

    commit

--- abc.java
+++ abc.java
@@ -4,5 +4,7 @@ class abc{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello, World! Devops");
+        
+        System.out.println("Hello, World! Devops reddy");
     }
 }
